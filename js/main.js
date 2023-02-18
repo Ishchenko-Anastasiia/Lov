9c58ee61c5f9b4d05ca77d1bc5fbc1720432ad02
Ishchenko Anastasiia <ishchenko.a17@gmail.com>
2023-02-18 15:59:37 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$('.slider').slick({
+    infinite: true,
+    arrows : false,
+    slidesToShow: 4,
+    autoplay: true,
+    slidesToScroll: 1, // по 1-му слайду проскролливать
+    speed: 3000, // медленная смена слайдов - скорость бегущей строки
+    autoplaySpeed: 0, //делаем запуск мгновенный с загрузкой страницы
+    cssEase: 'linear', // делаем анимацию однотонной при смене слайда
+    
+});
+
+$('.partners_list').slick({
+    infinite: true,
+    arrows : false,
+    slidesToShow: 3,
+    autoplay: true,
+    slidesToScroll: 1, // по 1-му слайду проскролливать
+    speed: 8000, // медленная смена слайдов - скорость бегущей строки
+    autoplaySpeed: 0, //делаем запуск мгновенный с загрузкой страницы
+    cssEase: 'linear', // делаем анимацию однотонной при смене слайда
+    
+});
+
+$('.blogers_list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+});
+
